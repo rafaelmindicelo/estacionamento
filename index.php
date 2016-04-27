5cc3064823f6d2553d22834463768325318c61be
gfinfo89 <biel_teck@hotmail.com>
2016-04-27 19:40:10 -0300

    Exclusão da Div postagem e alteração do ano do rodapé

--- index.php
+++ index.php
@@ -34,32 +34,10 @@
 				</ul>
 			</div>
 			<div id="div_Conteudo">
-				<div id="div_Postagem"> 
-				<?php
-					$verificar = $mysqli->query("SELECT * FROM postagens ORDER BY ID");
-					$Row = $verificar->num_rows;
-					
-					if ($Row <= 0) {
-						echo "Nenhuma mensagem foi postada!";
-					} else {
-						while ($array = $verificar->fetch_array()) {
-							$titulo = $array['Titulo'];
-							$texto = $array['Texto'];
-							$autor = $array['Autor'];
-							$data = $array['Data'];
-
-				?>
-				<h1> <b> Titulo: </b> <?php echo $titulo; ?> <b> Data: </b> <?php echo $data; ?> </h1>
-				<span> <b> Autor: </b> <?php echo $autor; ?> </span>
-				<p> <?php echo $texto; ?> </p>
-				<?php
-						}
-					}
-				?>	
-				</div>
+				
 			</div>	
 			<div id="div_Rodape"> 
-				<SPAN> Slater IT Developer's 2015 - Todos os direitos reservados. </SPAN>
+				<SPAN> Slater IT Developer's 2016 - Todos os direitos reservados. </SPAN>
 			</div>
 		</div>
 	</body>
